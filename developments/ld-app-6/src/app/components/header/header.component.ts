5b7297c9dd639b98c299d7715dec01b0f409e595
Daniel Dantas <contatodanieldantasdev@gmail.com>
2026-01-04 22:32:18 -0300

    feat: adição do arquivo header.component.ts

---
+++ developments/ld-app-6/src/app/components/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+
+export class HeaderComponent implements OnInit {
+
+  ngOnInit(): void {
+    // executa aqui quando a tela estiver carregada
+
+    // Proxima aula: Quando acessar /header -> entrar no Componente de Header
+  }
+
+}
